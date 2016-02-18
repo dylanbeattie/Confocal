00cda626793efaefecc7f50857266bdb6834f09d
Dylan Beattie <dylan.beattie@spotlight.com>
2016-02-18 13:48:33 +0000

    First UI prototype of feedback system spike.

---
+++ src/ConjaWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConjaWeb.Models;
+
+namespace ConjaWeb.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+            return View();
+        }
+    }
+
+    public class TalksController : Controller {
+        public ActionResult Feedback(string code) {
+            var data = new FeedbackViewData();
+            return (View(data));
+        }
+    }
+
+
+}
---
+++ src/ConjaWeb/Models/FeedbackViewData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConjaWeb.Models {
+    public class FeedbackViewData {
+        public string TalkHeadline { get; set; }
+    }
+}
